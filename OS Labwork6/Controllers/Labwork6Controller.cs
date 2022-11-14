9b0dfa09ae0ae1b73d8fccd22dbf6a0f6fbd02f6
Роберт <98408605+jrkmk45@users.noreply.github.com>
2022-11-14 17:47:10 +0300

    initial commit

---
+++ OS Labwork6/Controllers/Labwork6Controller.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OS_Labwork6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Labwork6Controller : ControllerBase
+    {
+
+        private readonly ILogger<Labwork6Controller> _logger;
+
+        public Labwork6Controller(ILogger<Labwork6Controller> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("Get")]
+        public ActionResult<string> Get()
+        {
+            string res = "Лабораторна робота 6. Виконав Кіш Роберт Робертович, студент 3 курсу ІПЗ";
+            return Ok(res);
+        }
+    }
+}
